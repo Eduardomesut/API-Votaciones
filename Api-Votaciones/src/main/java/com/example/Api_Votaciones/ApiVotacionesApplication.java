550ae146adaa3fcf72a214b3a989b5ee87ad9e8f
eduardomesut <eduardomesut@gmail.com>
2025-01-12 20:15:28 +0100

    Iniciación y nuevas instrucciones

--- Api-Votaciones/src/main/java/com/example/Api_Votaciones/ApiVotacionesApplication.java
+++ Api-Votaciones/src/main/java/com/example/Api_Votaciones/ApiVotacionesApplication.java
@@ -2,12 +2,17 @@ package com.example.Api_Votaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ApiVotacionesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiVotacionesApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(ApiVotacionesApplication.class, args);
+		//PersonalRepository repository1 = context.getBean(PersonalRepository.class);
+
+
 	}
 
 }

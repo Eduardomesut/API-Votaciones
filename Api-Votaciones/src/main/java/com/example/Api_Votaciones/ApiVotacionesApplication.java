ad891c1d3b959ae53ad1c24e0e580ce31e8a321c
eduardomesut <eduardomesut@gmail.com>
2025-01-12 19:35:36 +0100

    Inicio de api config

---
+++ Api-Votaciones/src/main/java/com/example/Api_Votaciones/ApiVotacionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Api_Votaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVotacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVotacionesApplication.class, args);
+	}
+
+}
---
+++ Api-Votaciones/src/test/java/com/example/Api_Votaciones/ApiVotacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Api_Votaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVotacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
